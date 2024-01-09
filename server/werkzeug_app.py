943aba46b269d037301f508264c090a3b649ea02
Emmi Lindsey <emmilindsey@gmail.com>
2024-01-08 18:14:52 -0700

    finished lab

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+from werkzeug.wrappers import Request, Response
+
+
+@Request.application
+def application(request):
+    print(f"This web server if running at {request.remote_addr}")
+    return Response('Hello beep borp I am a robot!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
